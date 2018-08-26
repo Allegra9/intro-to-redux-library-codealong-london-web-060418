c380a47df6a2f4e38f31e743ef466c7e43f3655a
Allegra <aukseallegra@gmail.com>
2018-08-26 22:32:50 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
 
-const store = createStore(shoppingListItemReducer);
+const store = createStore(shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
+  //this is for Chrome Extension
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,13 +1,15 @@
-export default function shoppingListItemReducer(state = {
-  items: []
-}, action) {
+const defaultState = {
+  items: [],
+}
+
+export default function shoppingListItemReducer(state = defaultState, action) {
   console.log(action);
   switch(action.type) {
 
     case 'INCREASE_COUNT':
       console.log("Current state.items length %s", state.items.length);
       console.log("Updating state.items length to %s", state.items.length + 1);
-      return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
+      return ({...state}, { items: state.items.concat(state.items.length + 1) });
 
     default:
       console.log("Initial state.items length: %s", state.items.length);
